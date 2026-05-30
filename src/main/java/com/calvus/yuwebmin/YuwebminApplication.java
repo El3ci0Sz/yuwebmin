3a95108fba0b84a5b139fe74c652c949587e0f78
El3ci0Sz <pedro.eleciof@gmail.com>
2026-05-30 00:11:22 -0300

    feat/create-spring-maven-project

---
+++ src/main/java/com/calvus/yuwebmin/YuwebminApplication.java
@@ -0,0 +1,13 @@
+package com.calvus.yuwebmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuwebminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YuwebminApplication.class, args);
+	}
+
+}
